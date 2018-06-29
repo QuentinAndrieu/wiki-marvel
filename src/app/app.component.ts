72eb5fac5f54c88b1986b43485bc3a7133d7c1c0
Andrieu Quentin <pro.andrieu.quentin@gmail.com>
2018-06-29 19:31:14 +1200

    Add heroku ping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(private http: Http) {
+    // Ping server every 5 minutes to keep heroku app active
+    setInterval(() => {
+      this.http.get('https://wiki-marvel.herokuapp.com/');
+      console.log('wiki');
+    }, 300000);
+  }
 }
