0d602ca83b4901ffc5e163cfb3720c8ac34ce20d
Andrieu Quentin <pro.andrieu.quentin@gmail.com>
2018-06-29 21:28:15 +1200

    Remove ping heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +7,5 @@ import { Http } from '@angular/http';
 })
 export class AppComponent {
   title = 'app';
-  constructor(private http: Http) {
-    // Ping server every 5 minutes to keep heroku app active
-    setInterval(() => {
-      this.http.get('https://wiki-marvel.herokuapp.com/');
-      console.log('wiki');
-    }, 300000);
-  }
+
 }
